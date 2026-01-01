efd4c7564c30b230cdc7fb73d75647bd3b8dfb46
prabhanjansp <puranikchetan97@gmail.com>
2026-01-01 23:21:29 +0530

    best time to buy and sell stocks problem solved

---
+++ JS_CORE/dasquestions/bestiTimeToSellAndBuyStocks.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {number[]} prices
+ * @return {number}
+ */
+//best time to buy and sell stocks
+var maxProfit = function (prices) {
+    let min = prices[0];
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] - min > maxProfit) {
+            maxProfit = prices[i] - min
+        }
+        if (prices[i] < min) {
+            min = prices[i]
+        }
+    }
+    return maxProfit
+
+
+
+
+};
